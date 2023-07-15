d914a5f03f339aee8b5cc81ca19fc5b0fb0b776c
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-07-15 12:46:58 +0000

    コメント追加

--- rpn.rb
+++ rpn.rb
@@ -1,6 +1,7 @@
 # 逆ポーランド記法の計算
 
 class String
+  # 正規表現オブジェクトを動的に生成
   RE_0 = Regexp.new('[=\s]+$')
   RE_1 = Regexp.new('\s+')
   RE_D = Regexp.new('\d+')
@@ -11,11 +12,14 @@ class String
 
   def rpn
     stack = []
+    # RE_0を""に置き換えて、RE_1で分割
     self.sub(RE_0, "").split(RE_1).each do |t|
+      # tがRE_Dにマッチしているか
       if t =~ RE_D
         stack.push(t.to_f)
         next
       end
+      # stackの末尾の要素を取り除いてそれを返す
       r, l = stack.pop, stack.pop
       case t
       when RE_PL; stack.push(l.to_f + r.to_f)
@@ -29,6 +33,7 @@ class String
 end
 
 if __FILE__ == $0
+  # .chompは 改行コードを取り除いた文字列を生成して返す
   f = $stdin.gets.chomp
   exit if f == ""
   puts f.rpn
